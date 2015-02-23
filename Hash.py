72cfce3b878fb488922d99a55a0b657f723b275a
ethanx94 <coolkid602007@gmail.com>
2015-02-23 17:09:16 -0600

    looking at bv functions

--- Hash.py
+++ Hash.py
@@ -1,5 +1,6 @@
 '''
 # Created by Ethan Richardson, Nathan Knight
+# written for Python3
 
 You are required to implement in Python a very simple hash function 
 (that is meant more for play than for any serious production work). 
@@ -8,7 +9,6 @@ Write a function that creates a 32-bit hash of a file
 through the following steps:
 
 
-Scan the file one byte at a time.
 Before a new byte is read from the file, 
 circularly shift the bit pattern in the hash to the left by 8 positions.
 Note that if there is no more data to read, 
@@ -17,21 +17,51 @@ XOR the new byte read from the file with
 the least significant byte (the rightmost) of the hash.
 Scan your directory (a very simple thing to do in Python), 
 and compute the hash of all your files.
-Dump the hash values in some output file.
+
 Hint: You can use BitVector Class
 
 '''
+#!/usr/bin/env python
 import sys
-import BitVector
+
+from BitVector import *
 
 def main():
 	#Initialize the hash to all zeros.
 	#This bit vector will hold exactly 32 bits, all initialized to the 0 bit value.
-	bv  = BitVector.BitVector(size = 32)
-	print (bv)
-	#file object = open(file_name [, access_mode][, buffering])
-	fo = open("testFile", "r+")
-	print (fo)
-	print ("Next Up: Scan the file one byte at a time!")
+	bv = BitVector(size = 32)
 	
+	bv = BitVector( filename = 'testFile.txt' )
+	while (bv.more_to_read):
+		#Scan the file one byte at a time.
+		bv1 = bv_read = bv.read_bits_from_file(32)
+		#Circularly shift bit pattern in hash to left by 8 pos.
+		bv1 << 8
+	print(bv_read)
+	
+	bv.close_file_object()
+	
+	'''
+	#Scan the file one byte at a time.
+	with open("testFile.txt", "rb") as f: #rb+ for read in binary
+		nextByte = f.read(1)
+		while nextByte:
+			#Circularly shift bit pattern in hash to left by 8 pos.
+			#bv += nextByte
+			bv << 8
+			nextByte = f.read(1)
+	
+	print (bv)
+	f.close
+	'''
+	
+	#Dump the hash values in some output file.
+	FILEOUT = open( 'output.txt', 'wb' )
+	bv1.write_to_file( FILEOUT )
+	FILEOUT.close()
+
+	print ("Next Up: Figure out correct method of file access to use.")
+	print ("open() or bitVector(filename =), to read ea file in one byte at a time") 
+	print ("Make this work with our empty 0-init vector") 
+
 main()
