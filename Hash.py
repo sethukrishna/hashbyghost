986de0cb8080b46180619030d0fa031a1bd8c0b7
ethanx94 <coolkid602007@gmail.com>
2015-02-23 17:59:46 -0600

    working on perfecting single file shifts

--- Hash.py
+++ Hash.py
@@ -1,5 +1,6 @@
 '''
 # Created by Ethan Richardson, Nathan Knight
+# written for Python3
 
 You are required to implement in Python a very simple hash function 
 (that is meant more for play than for any serious production work). 
@@ -8,30 +9,63 @@ Write a function that creates a 32-bit hash of a file
 through the following steps:
 
 
-Scan the file one byte at a time.
 Before a new byte is read from the file, 
 circularly shift the bit pattern in the hash to the left by 8 positions.
 Note that if there is no more data to read, 
 you stop here (i.e., after shifting).
 XOR the new byte read from the file with 
 the least significant byte (the rightmost) of the hash.
-Scan your directory (a very simple thing to do in Python), 
-and compute the hash of all your files.
-Dump the hash values in some output file.
+
+
 Hint: You can use BitVector Class
 
 '''
+#!/usr/bin/env python
 import sys
-import BitVector
+
+from BitVector import *
+from glob import *
 
 def main():
 	#Initialize the hash to all zeros.
 	#This bit vector will hold exactly 32 bits, all initialized to the 0 bit value.
-	bv  = BitVector.BitVector(size = 32)
-	print (bv)
-	#file object = open(file_name [, access_mode][, buffering])
-	fo = open("testFile", "r+")
-	print (fo)
-	print ("Next Up: Scan the file one byte at a time!")
+	myHash = BitVector(size = 32)
 	
+	#INCOMPLETE
+	###########################
+	#Scan your directory (current), compute the hash of all your files.
+	# TO EXCLUDE: PYCACHE, README,  
+	dirScan = glob('*')
+	fileCount = 0
+
+	print(dirScan)
+	#bv = BitVector( dirScan )
+	###########################
+
+	bv = BitVector( filename = 'testFile.txt' )
+	
+	#If there no more data to read,
+	#Stop after shifting
+	while (bv.more_to_read):
+		#Scan the file one byte at a time.
+		bv1 = bv_read = bv.read_bits_from_file(8)
+		print(bv_read)
+		#Circularly shift bit pattern in hash to left by 8 pos.
+		bv1 << 8
+
+	#XOR the new byte read from the file with 
+	#the least significant byte (the rightmost) of the hash.
+	myHash[0:8] = bv1 ^ myHash[0:8]
+	
+	print(bv_read)
+	
+	bv.close_file_object()
+		
+	'''
+	#Dump the hash values in some output file.
+	dumpFile = open('dump.txt', 'wb')
+	bv1.write_to_file(dumpFile)
+	dumpFile.close()
+	'''
+
 main()
