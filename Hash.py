cfe0f5b76e6c6ac30bbd158ecb68e0ba6ebb6b1c
ghostrooter <sethuvkrishna@gmail.com>
2016-03-15 04:22:36 +0000

    time pass

--- Hash.py
+++ Hash.py
@@ -1,56 +1,26 @@
-'''
-# Created by Ethan Richardson, Nathan Knight
-# written for Python3
-
-You are required to implement in Python a very simple hash function 
-(that is meant more for play than for any serious production work). 
-Write a function that creates a 32-bit hash of a file 
-(you can represent 32-bits in hexadecimal and use it as the output for simplicity)
-'''
-#!/usr/bin/env python
 import sys
-
-from BitVector import *
 from glob import *
-
-def main():
-
-	#Scan your directory (current), compute the hash of all your files.
+from BitVector import *
+def HashbyGhost():
 	dirScan = glob('input/*.*')
 	fileCount = 0
-
 	while(fileCount < len(dirScan)):
 		bv = BitVector(filename = dirScan[fileCount])
 		
-		#Initialize the hash to all zeros.
-		#This bit vector will hold exactly 32 bits, all initialized to the 0 bit value.
 		myHash = BitVector(size = 32)
 		
-		#If no more data to read,
-		#Stop after shifting
 		while (bv.more_to_read):
-			#Scan the file one byte (8bits) at a time.
 			bv1 = bv.read_bits_from_file(8)
-			
-			#XOR the new byte read from the file with 
-			#the least significant byte (the rightmost) of the hash.
 			myHash[0:8] = bv1 ^ myHash[0:8]
-			
-			#Circularly shift bit pattern in hash to left by 8 pos.
 			myHash << 8
 		
 		bv.close_file_object()
-			
-		#Convert to Hex
 		myHexHash = myHash.getHexStringFromBitVector()
-	
-		#Dump the hash values in some output file.
-		dumpFile = open('output/dump.txt', 'a')
+		dumpFile = open('output/output.txt', 'a')
 		dumpFile.write('\n')
 		dumpFile.write(dirScan[fileCount])
 		dumpFile.write(":")
 		dumpFile.write(myHexHash)
 		dumpFile.close()
 		fileCount += 1
-	
-main()
+HashbyGhost()
